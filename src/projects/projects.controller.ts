b3e7718fa7c15dce263e69f1fc15d90375100c2b
Md-Tahsif-Ahmed <tahsif.cse@gmail.com>
2025-08-16 13:47:02 +0600

    RBAC

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -14,6 +14,8 @@ import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { ProjectsService } from './projects.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
+import { UserRole } from 'src/users/entities/user.entity';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
 interface JwtUser {
   userId: string;
@@ -30,21 +32,25 @@ export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
 
   @Post()
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN)
   create(@Body() dto: CreateProjectDto, @Req() req: AuthRequest) {
     return this.projectsService.create(dto, req.user.userId);
   }
 
   @Get()
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.USER)
   findAll(@Req() req: AuthRequest) {
     return this.projectsService.findAll(req.user.userId);
   }
 
   @Get(':id')
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.USER)
   findOne(@Param('id') id: string, @Req() req: AuthRequest) {
     return this.projectsService.findOne(id, req.user.userId);
   }
 
   @Patch(':id')
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN)
   update(
     @Param('id') id: string,
     @Body() dto: UpdateProjectDto,
@@ -54,6 +60,7 @@ export class ProjectsController {
   }
 
   @Delete(':id')
+  @Roles(UserRole.SUPER_ADMIN)
   remove(@Param('id') id: string, @Req() req: AuthRequest) {
     return this.projectsService.remove(id, req.user.userId);
   }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,32 +6,43 @@ import {
   Param,
   Patch,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { Task } from './entities/task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskService } from './tasks.service';
 import { UpdateTaskDto } from './dto/update-task.dto';
 
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { UserRole } from 'src/users/entities/user.entity';
+
 @Controller('tasks')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @Post()
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN)
   create(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
     return this.taskService.create(createTaskDto);
   }
 
   @Get()
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.USER)
   findAll(): Promise<Task[]> {
     return this.taskService.findAll();
   }
 
   @Get(':id')
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.USER)
   findOne(@Param('id') id: string): Promise<Task> {
     return this.taskService.findOne(id);
   }
 
   @Patch(':id')
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN)
   update(
     @Param('id') id: string,
     @Body() updateTaskDto: UpdateTaskDto,
@@ -40,11 +51,13 @@ export class TaskController {
   }
 
   @Delete(':id')
+  @Roles(UserRole.SUPER_ADMIN)
   remove(@Param('id') id: string): Promise<void> {
     return this.taskService.remove(id);
   }
 
   @Get('project/:projectId/sorted')
+  @Roles(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.USER)
   getSortedTasks(@Param('projectId') projectId: string): Promise<Task[]> {
     return this.taskService.getTasksSortedByDependencies(projectId);
   }
